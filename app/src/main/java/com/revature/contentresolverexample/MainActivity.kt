9cd6720d4d5c0bf1be76cf04e0de94a3f7eb92d2
RyanKCox <ryankcox90@gmail.com>
2022-03-30 16:56:42 -0500

    done

--- app/src/main/java/com/revature/contentresolverexample/MainActivity.kt
+++ app/src/main/java/com/revature/contentresolverexample/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.revature.contentresolverexample
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,11 +20,22 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
 import com.revature.contentresolverexample.ui.theme.ContentResolverExampleTheme
 
+
 class MainActivity : ComponentActivity() {
+    companion object{
+
+        const val PROVIDER_NAME="com.revature.user.provider"
+        const val TABLE_NAME="users"
+
+        val CONTENT_URI = Uri.Builder()
+            .scheme(ContentResolver.SCHEME_CONTENT)
+            .authority(PROVIDER_NAME)
+            .appendPath(TABLE_NAME).build()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -35,10 +51,13 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@SuppressLint("Range")
 @Composable
 fun ResolverScreen(){
     val scaffoldState = rememberScaffoldState()
     val context = LocalContext.current
+    var sResult by rememberSaveable { mutableStateOf("") }
 
     Scaffold(
         scaffoldState = scaffoldState,
@@ -59,12 +78,34 @@ fun ResolverScreen(){
                         .fillMaxWidth(.6f),
                     onClick = {
 
+                        val cursor =
+                            context.contentResolver.query(
+                                MainActivity.CONTENT_URI,
+                            null,null,null,null
+                        )
+                        if ( cursor!!.moveToFirst()){
 
-                        Toast.makeText(
-                            context,
-                            "Users Fetched",
-                            Toast.LENGTH_SHORT
-                        ).show()
+
+
+                            while (!cursor.isAfterLast){
+
+
+                                sResult += "${cursor.getString(
+                                    cursor.getColumnIndex("id"))}-${cursor.getString(cursor.getColumnIndex("name"))}"
+                                cursor.moveToNext()
+                                sResult += "\n"
+                            }
+                            Log.d("Data", sResult)
+                        }
+                        else {
+                            Log.d("Data", "No Records Found")
+
+                            Toast.makeText(
+                                context,
+                                "Users Fetched",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
 
                     }) {
                     Text(text = "Fetch Users")
